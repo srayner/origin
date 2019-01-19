aeae31ed606d54b7e0faac83470b7dee97dca6d8
Steve Rayner <srayner02@gmail.com>
2019-01-19 20:04:20 +0000

    Added birth and death to person detials

--- src/components/person/person-detail.js
+++ src/components/person/person-detail.js
@@ -5,8 +5,14 @@ import Label from "../ui/label";
 import Radio from "../ui/radio";
 
 class PersonDetail extends React.Component {
+  dateAsText(dateObject) {
+    return dateObject.day + " " + dateObject.month + " " + dateObject.year;
+  }
+
   render() {
     const { person } = this.props;
+    const birth = this.dateAsText(person.birth);
+    const death = this.dateAsText(person.death);
     return (
       <div>
         <Label>Forenames</Label>
@@ -23,6 +29,14 @@ class PersonDetail extends React.Component {
         >
           Female
         </Radio>
+        <Label>Birth Date</Label>
+        <Text value={birth} />
+        <Label>Birth Place</Label>
+        <Text value={person.birth.place} />
+        <Label>Death Date</Label>
+        <Text value={death} />
+        <Label>Death Place</Label>
+        <Text value={person.death.place} />
       </div>
     );
   }

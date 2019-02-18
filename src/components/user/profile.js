6fb179813fbfd10fd6a273ea7c1775cfd8c65dfc
Steve Rayner <srayner02@gmail.com>
2019-02-18 21:52:49 +0000

    profile page now redirects if not logged in.

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import Panel from "../ui/panel";
 import Label from "../ui/label";
 import Text from "../ui/text";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 const Container = styled.div`
   margin: 0;
@@ -10,26 +12,42 @@ const Container = styled.div`
   background-color: #eee;
 `;
 
-const Profile = props => {
-  return (
-    <Container>
-      <h1>Your profile</h1>
-      <Panel>
-        <div>
-          <Label>Display Name</Label>
-          <Text value="Steve Rayner" />
-        </div>
-        <div>
-          <Label>Initials</Label>
-          <Text value="S R" />
-        </div>
-        <div>
-          <Label>Email</Label>
-          <Text value="myname@mydomain.com" />
-        </div>
-      </Panel>
-    </Container>
-  );
+class Profile extends React.Component {
+  render() {
+    const { token } = this.props;
+    if (!token) {
+      return <Redirect to="/login" />;
+    }
+    console.log("Componnet rendered");
+    return (
+      <Container>
+        <h1>Your profile</h1>
+        <Panel>
+          <div>
+            <Label>Display Name</Label>
+            <Text value="Steve Rayner" />
+          </div>
+          <div>
+            <Label>Initials</Label>
+            <Text value="S R" />
+          </div>
+          <div>
+            <Label>Email</Label>
+            <Text value="myname@mydomain.com" />
+          </div>
+        </Panel>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    token: state.app.token
+  };
 };
 
-export default Profile;
+export default connect(
+  mapStateToProps,
+  null
+)(Profile);

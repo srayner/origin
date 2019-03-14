e52708a1629312adf7e3d21fecdc590ed87e2cee
Steve Rayner <srayner02@gmail.com>
2019-03-14 22:10:52 +0000

    Removed unused import.

--- src/library/tree.js
+++ src/library/tree.js
@@ -1,10 +1,8 @@
 import buildTree from "./build-tree";
-import tidyTree from "./tidy-tree";
 import drawTree from "./draw-tree";
 
 export default function updateTree(people, families, clickHandler) {
   const tree = { people, families, pitchX: 100, pitchY: 200 };
   buildTree(tree);
-  //  tidyTree(tree);
   drawTree(tree, clickHandler);
 }
